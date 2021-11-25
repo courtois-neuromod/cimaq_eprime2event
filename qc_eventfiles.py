9579067404d4c7958f13555972fb0ff82ea797c8
MarieStLaurent <marie.stl@gmail.com>
2021-11-25 11:33:54 -0500

    first qc commit

--- qc_eventfiles.py
+++ qc_eventfiles.py
@@ -23,12 +23,12 @@ def get_arguments():
 
     parser.add_argument(
         "-d", "--idir",
-        required=True, nargs="+",
+        required=True,
         help="Directory that contains event files (.tsv)")
 
     parser.add_argument(
         "-o", "--odir",
-        required=True, nargs="+",
+        required=True,
         help="Output directory - if doesn\'t exist it will be created.")
 
     args = parser.parse_args()
@@ -39,10 +39,25 @@ def get_arguments():
         return args
 
 
+def get_all_tsvs(in_dir):
+    pass
+    
+
 def main():
     '''
     This is where the magic happens!!
     '''
+    args = get_arguments()
+
+    input_dir = args.idir
+    output_dir = args.odir
+
+    # Create output_dir if not exists
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    tsv_list = get_all_tsvs(input_dir)
+
     pass
 
 
