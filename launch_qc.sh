8add38213258e272fb0a297ec60a5cf826cee176
arnaudbore <arnaud.bore@gmail.com>
2021-11-25 15:37:04 -0500

    Revert "[WIP][ENH] Add QC script"

--- launch_qc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INPUT_FOLDER="/home/labopb/Documents/Marie/neuromod/CIMAQ/event_files/V03/taskfiles"
-OUTPUT_FOLDER="/home/labopb/Documents/Marie/neuromod/CIMAQ/cimaq_eprime2event/test"
-
-# launch job
-python -m qc_eventfiles \
-        --idir "${INPUT_FOLDER}" \
-        --odir "${OUTPUT_FOLDER}"
